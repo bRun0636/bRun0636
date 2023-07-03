f6d2c6f24aaa7c3529ad9a9b04ee61f42cab16b6
bRun0636 <137895656+bRun0636@users.noreply.github.com>
2023-07-03 21:51:08 +0300

    Add files via upload

---
+++ Tower/Tower/Person.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tower
+{
+        abstract class Person
+        {
+            protected Random rnd = new Random();
+            protected int hp, lvl;
+            protected string name;
+            protected bool IsDeath;
+            public bool Death()
+            {    // проверяет мертв ли персонаж
+                return IsDeath = hp <= 0;
+            }
+            public int Hp
+            {            // показывает сколько здоровья
+                get { return hp; }
+                set { hp = value; }
+            }
+            public int Lvl
+            {
+                get { return lvl; }
+            }
+        }
+        internal class Player : Person
+        {
+            Weapon sword;
+            Weapon bow;
+            int stats, xp, luck;
+            int max_hp, max_xp;
+            double critical_damage, dodge;
+            bool weap;
+            string name;
+            public Player(string name)
+            {
+                this.name = name;           //имя персонажа
+                IsDeath = false;            //булевая переменная, показывает умер ли игрок
+                hp = 100;
+                dodge = rnd.Next(10, 30);   //шанс уклона
+                xp = 0;                     //опыт
+                lvl = 1;                    //уровень
+                critical_damage = 1.2;      //коэффицент крит. урона
+                luck = rnd.Next(10, 20);    //удача(шанс крит. урона)
+                stats = 0;                  //очки повышения характеристек
+                max_hp = 100;
+                max_xp = 50;
+                sword = new Sword(0);       //первоначальное оружие
+                weap = true;                //показывает какое оружие меч/лук
+            }
+            public bool Dodge()
+            {
+                return dodge > rnd.Next(1, 100);
+            }
+            public string Description()
+            {    //описание персонажа
+                if (bow != null)
+                {
+                    return $"Имя: {name}\nЗдоровье:{hp}\nУровень:{lvl}\nОпыт:{xp}\nСвободные очки прокачки:{stats}\nОружие:\n{sword.Name} {sword.Rare} класса \n{bow.Name} {bow.Rare} класса";
+                }
+                else
+                {
+                    return $"Имя: {name}\nЗдоровье:{hp}\nУровень:{lvl}\nОпыт:{xp}\nСвободные очки прокачки:{stats}\nОружие:\n{sword.Name} {sword.Rare} класса";
+                }
+            }
+            public Weapon Sword
+            {            //замена меча
+                get { return sword; }
+                set { sword = value; }
+            }
+            public Weapon Bow
+            {              //замена лука
+                get { return bow; }
+                set { bow = value; }
+            }
+            public void LvL_up(int n)
+            {      //получение опыта и повышения уровня
+                if (n < 5)
+                {
+                    xp += rnd.Next(10, 40);
+                }
+                else if (n > 5 && n < 9)
+                {
+                    xp += rnd.Next(40, 80);
+                }
+                else if (n == 5)
+                {
+                    xp += 100;
+                }
+                else if (n == 9)
+                {
+                    xp += 250;
+                }
+                else if (n == 10)
+                {
+                    xp += 500;
+                }
+                if (xp >= max_xp)
+                {
+                    xp -= max_hp;
+                    max_hp += 50;
+                    stats += 3;
+                    lvl += 1;
+                }
+            }
+            public void Stats_up(int n)
+            {    //повышения здоровья/выносливости/удачи/ловкости
+                switch (n)
+                {
+                    case 1:
+                        max_hp += 30;
+                        hp = max_hp;
+                        stats--;
+                        break;
+                    case 2:
+                        luck += rnd.Next(5, 10);
+                        stats--;
+                        break;
+                    case 3:
+                        dodge += rnd.Next(5, 10);
+                        stats--;
+                        break;
+                }
+            }
+            public void Out_damage(Enemy e)
+            { //урон врагу
+                if (weap)
+                {
+                    if (luck <= rnd.Next(1, 100))
+                    {
+                        e.Hp -= sword.Damage;
+                    }
+                    else
+                    {
+                        e.Hp -= Convert.ToInt32(sword.Damage * critical_damage);
+                    }
+                }
+                else
+                {
+                    if (luck <= rnd.Next(1, 100))
+                    {
+                        e.Hp -= bow.Damage;
+                    }
+                    else
+                    {
+                        e.Hp -= Convert.ToInt32(bow.Damage * critical_damage);
+                    }
+                }
+            }
+            public void In_damage(Enemy e)
+            { //получение урона
+                hp -= e.Damage;
+            }
+            public void Sleep() //восстановвление здоровья
+            {
+                hp = max_hp;
+            }
+        }
+    internal class Enemy : Person 
+    { 
+        int damage; 
+        List<string> enemies = new List<string>() { "Гаргамель", "Леший", "Жвачник", "Вампир", "Баба Яга" };
+        public Enemy(Player p, int n) 
+        { 
+            IsDeath = false; 
+            name = enemies[n - 1]; 
+            hp = rnd.Next(-30, 30) + p.Hp; if (p.Lvl > 4) 
+            { 
+                lvl = p.Lvl + rnd.Next(-3, 3); 
+            }
+            else 
+            { 
+                lvl = p.Lvl;
+            } 
+            damage = rnd.Next(5, 15) * lvl; 
+        } 
+        public int Damage 
+        { 
+            get 
+            { 
+                return damage; 
+            }
+        }
+    }
+
+}
+
---
+++ Tower/Tower/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Mime;
+using System.Dynamic;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tower
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int num_floor = 1, choice = 0;
+            Menu menu = new Menu();
+            Console.WriteLine("Добро пожаловать в Башню. Нам придется пройти 5 этажей, чтобы выбраться из нее. Давайте познакомимся! \nКак Вас зовут?");
+            Console.WriteLine();
+            string name = Console.ReadLine();
+            Player player = new Player(name);
+            Console.WriteLine($"Приятно познакомиться{name}.\nЯ Эса, Ваш интерактивный помощник, который расскажет, что Вас будет ждать на каждом этаже.\nНу что, попытаемся сбежать? ");
+            Console.WriteLine("1.Давай попробуем, вдруг получится) \n2.Нет, слишком сложно. Давай останемся тут, вдруг нас спасут. ");
+            while (true)
+            {
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice == 1 || choice == 2)
+                        break;
+                }
+                catch
+                {
+                    Console.WriteLine("Введите число.");
+                }
+            }
+            if (choice == 2)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+
+                Console.WriteLine("Держи меч, он поможет в прохождении!");
+                player.Sword.description();
+                Console.WriteLine("Мой дорогой друг, мы с тобой на первом этаже. Тут нас встречает деревня смурфов.\nОй-ой, злой волшебник Гаргамель. Заклятый враг смурфов. \nПостоянно предпринимает безуспешные попытки захватить смурфиков. Как он тут оказался? \nДавай победим его и спасем смурфов!");
+                Console.WriteLine( menu.fight());
+                while (true)
+                {
+                    try
+                    {
+                        choice = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Введите число.");
+                    }
+                }
+            }
+        }
+    }
+}
